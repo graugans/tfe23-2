87e3b4fd8874ee1439a244bdbc074e08326ac81e
Christian Ege <ch@ege.io>
2023-10-15 15:41:39 +0200

    chore: 🧹apply the clang format style

--- templates/json_parsing/main.cpp
+++ templates/json_parsing/main.cpp
@@ -1,8 +1,9 @@
-#include <fmt/format.h>
 #include <fmt/chrono.h>
-#include "CLI/CLI.hpp"
+#include <fmt/format.h>
+
 #include <nlohmann/json.hpp>
 
+#include "CLI/CLI.hpp"
 #include "config.h"
 
 // for convenience
@@ -10,27 +11,29 @@ using json = nlohmann::json;
 
 int main(int argc, char **argv)
 {
-  /**
-   * CLI11 is a command line parser to add command line options
-   * More info at https://github.com/CLIUtils/CLI11#usage
-   */
-  CLI::App app{ PROJECT_NAME };
-  try {
-    app.set_version_flag("-V,--version", fmt::format("{} {}",PROJECT_VER,PROJECT_BUILD_DATE));
-    app.parse(argc, argv);
-  } catch (const CLI::ParseError &e) {
-    return app.exit(e);
-  }
+    /**
+     * CLI11 is a command line parser to add command line options
+     * More info at https://github.com/CLIUtils/CLI11#usage
+     */
+    CLI::App app{PROJECT_NAME};
+    try
+    {
+        app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
+        app.parse(argc, argv);
+    }
+    catch (const CLI::ParseError &e)
+    {
+        return app.exit(e);
+    }
 
-  /**
-   * The {fmt} lib is a cross platform library for printing and formatting text
-   * it is much more convenient than std::cout and printf
-   * More info at https://fmt.dev/latest/api.html
-   */
-  fmt::print("Hello, {}!\n", app.get_name());
+    /**
+     * The {fmt} lib is a cross platform library for printing and formatting text
+     * it is much more convenient than std::cout and printf
+     * More info at https://fmt.dev/latest/api.html
+     */
+    fmt::print("Hello, {}!\n", app.get_name());
 
+    /* INSERT YOUR CODE HERE */
 
-  /* INSERT YOUR CODE HERE */
-
-  return 0; /* exit gracefully*/
+    return 0; /* exit gracefully*/
 }
