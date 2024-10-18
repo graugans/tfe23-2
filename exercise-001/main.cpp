22b514f7f3816486e1b5919e6bd59aa67c785c1e
Filip Langer <filiplanger@MacBook-Air-von-Filip.local>
2024-10-18 08:36:21 +0200

    idk what iam doing

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -11,9 +11,11 @@ auto main(int argc, char **argv) -> int
      * More info at https://github.com/CLIUtils/CLI11#usage
      */
     CLI::App app{PROJECT_NAME};
+    int counter = 20;
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
+        app.add_option("-c,--count", counter, "How many items to insert");
         app.parse(argc, argv);
     }
     catch (const CLI::ParseError &e)
@@ -28,7 +30,7 @@ auto main(int argc, char **argv) -> int
      */
     fmt::print("Hello, {}!\n", app.get_name());
 
-    /* INSERT YOUR CODE HERE */
+
 
     return 0; /* exit gracefully*/
 }
