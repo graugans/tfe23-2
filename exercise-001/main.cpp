622ea287341ba2de9d2c4a44aae9ffc9efe47610
AngusBach <156666508+AngusBach@users.noreply.github.com>
2024-10-18 08:42:45 +0200

    I Dont knowgit add .!
    owdngeiuoregf

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -10,10 +10,15 @@ auto main(int argc, char **argv) -> int
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
      */
+    inline CLR::Option *CL
+
+    int counter = 20;
     CLI::App app{PROJECT_NAME};
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
+        app.add_option("-c,--count", counter, "How many items to insert");
+        
         app.parse(argc, argv);
     }
     catch (const CLI::ParseError &e)
@@ -21,6 +26,7 @@ auto main(int argc, char **argv) -> int
         return app.exit(e);
     }
 
+        //int foo = counter; //undifiened, int count has be outside try-block
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
