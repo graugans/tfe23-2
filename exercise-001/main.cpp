b2827c7970c634930977b8eec0bd70b0837943c3
Christian Ege <226893+christianege@users.noreply.github.com>
2024-10-25 09:13:55 +0200

    feat: added the solution for Exercise 1

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -1,19 +1,20 @@
 #include <fmt/chrono.h>
 #include <fmt/format.h>
 
+#include <algorithm> /* needed for the std::sort */
+#include <random>
+#include <vector> /* Does provide std::vector */
+
 #include "CLI/CLI.hpp"
 #include "config.h"
 
-#include <vector> /* Does provide std::vector */
-#include <random>
-
-auto main(int argc, char **argv) -> int
+auto main(int argc, char** argv) -> int
 {
     /**
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
      */
-    auto counter{20}; /* set a default of 20 */
+    auto counter{20};           /* set a default of 20 */
     CLI::App app{PROJECT_NAME}; /* create an instance of an App with the variable name app*/
     try
     {
@@ -22,69 +23,46 @@ auto main(int argc, char **argv) -> int
         app.add_option("-c,--count", counter, "How many items to insert");
         app.parse(argc, argv);
     }
-    catch (const CLI::ParseError &e)
+    catch (const CLI::ParseError& e)
     {
         return app.exit(e);
     }
 
-    /**
-     * The {fmt} lib is a cross platform library for printing and formatting text
-     * it is much more convenient than std::cout and printf
-     * More info at https://fmt.dev/latest/api.html
-     */
-    fmt::print("Hello, {}!\n", app.get_name());
-    fmt::print("Value of argc, {}!\n", argc);
-    fmt::print("Value of argv[0], {}!\n", argv[0]);
-    // fmt::print("Value of argv[3], {}!\n", argv[3]); /* possible null pointer exception */
-    fmt::print("The value of counter {}!\n", counter);
-
-    std::vector<unsigned int> values{23,54,66};
-    fmt::print("Elements in values, {}!\n", values.size());
-    values.push_back(42);
-    values.push_back(4711);
-    values.push_back(11);
-    fmt::print("Elements in values, {}!\n", values.size());
-    for(int i = 0; i < values.size(); i++) {
-        fmt::print("Value of element {} in values: {}!\n", i, values[i]);
-    }
-    for (const unsigned int & value : values) { // access by const reference
-        fmt::print("Value of element {} !\n", value);
-    }
-
-    fmt::print("Value of none existing element 10 element {} !\n", values[10]);
-    // Throws an exception
-    //fmt::print("Value of none existing element 10 element  with at(): {} !\n", values.at(10));
-
+    
     // Seed with a real random value, if available
     std::random_device r;
- 
+
     // Choose a random mean between 1 and 100
     std::default_random_engine e1(r());
     std::uniform_int_distribution<int> uniform_dist(1, 100);
     int rand_value = uniform_dist(e1);
 
-    fmt::print("Random number between 1 and 100: {} !\n", rand_value);
- 
     std::vector<unsigned int> numbers;
-    for(int i = 0; i < counter; i++) {
+    auto start = std::chrono::system_clock::now();
+    for (int i = 0; i < counter; i++)
+    {
         numbers.push_back(uniform_dist(e1));
     }
+    auto end = std::chrono::system_clock::now();
 
-    for ( const unsigned int& xyz : numbers) { // range based for loop
-        // value = 0; /* not allowed due to const */
-        fmt::print("Value of element {} in numbers!\n", xyz);
-    }
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    fmt::println("The inserting took: {}", elapsed);
+    //fmt::println("The random vector: [ {} ]", fmt::join(numbers, ", "));
 
-    for(int i = 0; i < numbers.size(); i++) {
-        
-        fmt::print("Value of element {} in numbers: {}!\n", i, numbers[i]);
-        numbers[i] = 0; /* possible to modify the underlying vector entries */
-    }
 
-   for ( const unsigned int& xyz : numbers) { // range based for loop
-        // value = 0; /* not allowed due to const */
-        fmt::print("Value of element {} in numbers!\n", xyz);
-    }
-        
+    fmt::println("Let's sort the numbers vector");
+    fmt::println("--------------------------------------------------------------------------");
+
+    start = std::chrono::system_clock::now();
+    std::sort(numbers.begin(), numbers.end(), std::less<int>());
+    end = std::chrono::system_clock::now();
+
+    fmt::println("The sorted numbers vector");
+    fmt::println("--------------------------------------------------------------------------");
+
+
+    //fmt::println("The sorted vector: [ {} ]", fmt::join(numbers, ", "));
+    elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    fmt::println("The sorting took: {}", elapsed);
     return 0; /* exit gracefully*/
 }
